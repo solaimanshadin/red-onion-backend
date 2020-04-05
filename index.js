ca4b95bbb912268cab5996af2bb5fd406a68cbec
Solaiman Shadin <solaimanshadin@gmail.com>
2020-04-05 10:34:33 +0600

    Optional Port added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const port = 3200;
+const port = process.env.PORT || 3200;
 
 const uri = process.env.DB_PATH;
 let client = new MongoClient(uri , {useNewUrlParser:true});
